79f033d5e8e09edd15441daab221e6eb8d733fd0
Romain Rousseau <romain.rousseau5@gmail.com>
2019-06-28 03:00:13 +0200

    [ADD] Account creation page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
   { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'create-account', loadChildren: './pages/create-account/create-account.module#CreateAccountPageModule' },
 ];
 
 @NgModule({
