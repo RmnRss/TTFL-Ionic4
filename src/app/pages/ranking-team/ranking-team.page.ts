0669f89a501ba1e133e9f0e87deee5210d7dc4dc
Romain Rousseau <romain.rousseau5@gmail.com>
2019-06-28 03:26:50 +0200

    [ADD] Team Ranking Page

---
+++ src/app/pages/ranking-team/ranking-team.page.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+import {TTFLService} from '../../services/api/ttfl.service';
+import {UserService} from '../../services/api/user.service';
+import {TTFLTeam} from '../../../class/TTFL/TTFLTeam';
+
+@Component({
+  selector: 'app-ranking-team',
+  templateUrl: './ranking-team.page.html',
+  styleUrls: ['./ranking-team.page.scss'],
+})
+export class RankingTeamPage implements OnInit {
+
+  userTeam: TTFLTeam;
+  teams: Array<TTFLTeam>;
+
+  constructor(
+      public ttflService: TTFLService,
+      public userService: UserService) {
+  }
+
+  ngOnInit() {
+    this.ttflService.getTeamsRankingPromise().then(
+        response => {
+          this.teams = response.teams;
+        }, error => {
+          console.log(error);
+        });
+
+    this.ttflService.getTeamOfUser(this.userService.user)
+        .then(result => {
+          this.userTeam = result;
+        }, error => {
+          console.log(error);
+        });
+  }
+
+}
