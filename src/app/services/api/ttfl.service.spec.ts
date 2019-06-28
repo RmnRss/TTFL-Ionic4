9e0a39f057f2c2a2900204e6e44ce421b96ad2e3
Romain Rousseau <romain.rousseau5@gmail.com>
2019-06-28 02:05:39 +0200

    [ADD] TTFL Service

---
+++ src/app/services/api/ttfl.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { TTFLService } from './ttfl.service';
+
+describe('TTFLService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: TTFLService = TestBed.get(TTFLService);
+    expect(service).toBeTruthy();
+  });
+});
