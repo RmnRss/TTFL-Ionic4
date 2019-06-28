c927ad3af303de1aa54687f8c970d0ce1a467947
Romain Rousseau <romain.rousseau5@gmail.com>
2019-06-28 02:32:12 +0200

    [ADD] Side Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {MenuController, Platform} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
+import {Router} from '@angular/router';
+import {UserService} from './services/api/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+
   constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+      private platform: Platform,
+      private splashScreen: SplashScreen,
+      private statusBar: StatusBar,
+      private router: Router,
+      private menu: MenuController,
+      public userService: UserService
   ) {
     this.initializeApp();
   }
@@ -22,5 +28,24 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  openPage(destination: string) {
+    this.menu.toggle();
+    this.router.navigate(['/' + destination]);
+  }
+
+  logOut() {
+    // TODO : Add logout promise & fix logout
+    this.openPage('login');
+  }
+
+  openTeamPage() {
+    this.menu.toggle();
+    if (this.userService.userHasTeam()) {
+      this.router.navigate(['/team']);
+    } else {
+      this.router.navigate(['/create-or-join-team']);
+    }
   }
 }
