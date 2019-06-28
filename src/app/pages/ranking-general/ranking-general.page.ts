9dc908ed5e6cc5eb7d40192ffe2a38620dae374e
Romain Rousseau <romain.rousseau5@gmail.com>
2019-06-28 03:26:32 +0200

    [ADD] General Ranking Page

---
+++ src/app/pages/ranking-general/ranking-general.page.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit} from '@angular/core';
+import {User} from '../../../class/TTFL/user';
+import {TTFLService} from '../../services/api/ttfl.service';
+import {UserService} from '../../services/api/user.service';
+
+@Component({
+  selector: 'app-ranking-general',
+  templateUrl: './ranking-general.page.html',
+  styleUrls: ['./ranking-general.page.scss'],
+})
+export class RankingGeneralPage implements OnInit {
+
+  users: Array<User>;
+
+  constructor(public ttflService: TTFLService,
+              public userService: UserService) {
+  }
+
+  ngOnInit() {
+    this.ttflService.getUsersRankingPromise().then(
+        response => {
+          this.users = response.users;
+        }, error => {
+          console.log(error);
+        });
+  }
+
+}
