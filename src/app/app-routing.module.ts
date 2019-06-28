0df443d893e6f7ca1d250a17f679e0a3108f9d44
Romain Rousseau <romain.rousseau5@gmail.com>
2019-06-28 03:28:11 +0200

    [FIX] Missing paths to pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,13 @@ const routes: Routes = [
   { path: 'nba-daily-games', loadChildren: './pages/nba-daily-games/nba-daily-games.module#NbaDailyGamesPageModule' },
   { path: 'nba-player-stats', loadChildren: './pages/nba-player-stats/nba-player-stats.module#NbaPlayerStatsPageModule' },
   { path: 'nba-team-roster', loadChildren: './pages/nba-team-roster/nba-team-roster.module#NbaTeamRosterPageModule' },
+  { path: 'ranking-daily-picks', loadChildren: './pages/ranking-daily-picks/ranking-daily-picks.module#RankingDailyPicksPageModule' },
+  { path: 'ranking-general', loadChildren: './pages/ranking-general/ranking-general.module#RankingGeneralPageModule' },
+  { path: 'ranking-team', loadChildren: './pages/ranking-team/ranking-team.module#RankingTeamPageModule' },
+  { path: 'rules', loadChildren: './pages/rules/rules.module#RulesPageModule' },
+  { path: 'team', loadChildren: './pages/team/team.module#TeamPageModule' },
+  { path: 'settings', loadChildren: './pages/settings/settings.module#SettingsPageModule' },
+  { path: 'user-profile', loadChildren: './pages/user-profile/user-profile.module#UserProfilePageModule' },
 ];
 
 @NgModule({
